1efd212a9068f031b2893c33e7b110afb7e8f9c7
Igor-Proh <71402291+Igor-Proh@users.noreply.github.com>
2022-05-02 17:50:22 +0300

    Add files via upload

---
+++ project_springboot_demo_01_04_SpringBootStarterActuator/src/main/java/com/prokhnov/spring/springboot/spring_course_springboot/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.prokhnov.spring.springboot.spring_course_springboot.dao;
+
+
+import com.prokhnov.spring.springboot.spring_course_springboot.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
